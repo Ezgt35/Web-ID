8858ede319f3c8a6253febf1b6cdb0c8a4c940f8
Ezgt35 <bangezgt@gmail.com>
2025-04-23 17:15:39 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  let room = null;
+
+  socket.on('join', (roomId) => {
+    room = roomId;
+    socket.join(room);
+  });
+
+  socket.on('message', (msg) => {
+    if (room) {
+      socket.to(room).emit('message', msg);
+    }
+  });
+
+  socket.on('disconnect', () => {
+    if (room) socket.leave(room);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server listening on http://localhost:3000');
+});
